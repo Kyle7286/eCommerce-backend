52ec20939332ef0ae836631b81aeb46e2600c628
Brandon Haley <kyle7286@gmail.com>
2021-03-18 16:55:20 -0400

    connection.js setup and configured

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,19 @@
+// For creating the DB  connection
+const Sequelize = require('sequelize');
+// For hiding sensitive information externally via .env
 require('dotenv').config();
 
-const Sequelize = require('sequelize');
 
+// Create DB connection with heroku support
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
+      host: process.env.DB_HOST,
       dialect: 'mysql',
       dialectOptions: {
         decimalNumbers: true,
       },
     });
 
+// Export the connection for use in server.js
 module.exports = sequelize;

8159a20123727c3f71c6f5e68a6400471719784a
Brandon Haley <kyle7286@gmail.com>
2021-03-19 15:42:58 -0400

    completed tagdata /:id route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -18,20 +18,33 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findAll({
+      where: {
+        id: req.params.id
+      },
+      include: [{
+        model: Product,
+      }]
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
 });
 

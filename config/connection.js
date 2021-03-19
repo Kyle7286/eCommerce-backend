d14272168ff7bd070028cabe4c3388936ea0e705
Brandon Haley <kyle7286@gmail.com>
2021-03-19 09:18:43 -0400

    completed model index.js association defining

--- config/connection.js
+++ config/connection.js
@@ -8,12 +8,12 @@ require('dotenv').config();
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: process.env.DB_HOST,
-      dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    dialectOptions: {
+      decimalNumbers: true,
+    },
+  });
 
 // Export the connection for use in server.js
 module.exports = sequelize;
